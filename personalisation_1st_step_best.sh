3e60a1714e80ca83247c88a5eb3c38449b5d364d
GP <gpuhuang@gmail.com>
2023-12-27 20:40:16 +0200

    add line breakders

--- personalisation_1st_step_best.sh
+++ personalisation_1st_step_best.sh
@@ -12,40 +12,64 @@ source activate data2vec
 ### AROUSAL
 
 # egemaps
-python3 main.py --task personalisation --feature egemaps --normalize --emo_dim physio-arousal --model_dim 256 --rnn_n_layers 4 --lr 0.002  --win_len 50 --hop_len 25 --rnn_dropout 0.5 --use_gpu
+python3 main.py --task personalisation --feature egemaps --normalize \
+            --emo_dim physio-arousal --model_dim 256 --rnn_n_layers 4 --lr 0.002 \
+            --win_len 50 --hop_len 25 --rnn_dropout 0.5 --use_gpu
 
 # deepspectrum
-python3 main.py --task personalisation --feature ds --emo_dim physio-arousal --model_dim 32 --rnn_n_layers 2 --lr 0.005  --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature ds \
+            --emo_dim physio-arousal --model_dim 32 --rnn_n_layers 2 --lr 0.005 \
+            --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
 
 # w2v
-python3 main.py --task personalisation --feature w2v-msp --emo_dim physio-arousal --model_dim 32 --rnn_n_layers 4 --rnn_bi --lr 0.005  --win_len 200 --hop_len 100 --rnn_dropout 0.5 --use_gpu
+python3 main.py --task personalisation --feature w2v-msp \
+            --emo_dim physio-arousal --model_dim 32 --rnn_n_layers 4 --rnn_bi --lr 0.005  \
+            --win_len 200 --hop_len 100 --rnn_dropout 0.5 --use_gpu
 
 # faus
-python3 main.py --task personalisation --feature faus --emo_dim physio-arousal --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.005  --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature faus \
+            --emo_dim physio-arousal --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.005  \
+            --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
 
 # vit
-python3 main.py --task personalisation --feature vit --normalize --emo_dim physio-arousal --model_dim 256 --rnn_n_layers 4 --rnn_bi --lr 0.005  --win_len 50 --hop_len 25 --rnn_dropout 0.5 --use_gpu
+python3 main.py --task personalisation --feature vit --normalize \
+            --emo_dim physio-arousal --model_dim 256 --rnn_n_layers 4 --rnn_bi --lr 0.005  \
+            --win_len 50 --hop_len 25 --rnn_dropout 0.5 --use_gpu
 
 # facenet
-python3 main.py --task personalisation --feature facenet --emo_dim physio-arousal --model_dim 256 --rnn_n_layers 1 --lr 0.001  --win_len 50 --hop_len 25 --rnn_dropout 0.5 --use_gpu
+python3 main.py --task personalisation --feature facenet \
+            --emo_dim physio-arousal --model_dim 256 --rnn_n_layers 1 --lr 0.001  \
+            --win_len 50 --hop_len 25 --rnn_dropout 0.5 --use_gpu
 
 
 ### VALENCE
 
 # egemaps
-python3 main.py --task personalisation --feature egemaps --normalize --emo_dim valence --model_dim 256 --rnn_n_layers 4 --lr 0.002  --win_len 200 --hop_len 100 --rnn_dropout 0.5 --use_gpu
+python3 main.py --task personalisation --feature egemaps --normalize \
+            --emo_dim valence --model_dim 256 --rnn_n_layers 4 --lr 0.002  \
+            --win_len 200 --hop_len 100 --rnn_dropout 0.5 --use_gpu
 
 # deepspectrum
-python3 main.py --task personalisation --feature ds --emo_dim valence --model_dim 64 --rnn_n_layers 2 --lr 0.001  --win_len 100 --hop_len 50 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature ds \
+            --emo_dim valence --model_dim 64 --rnn_n_layers 2 --lr 0.001  \
+            --win_len 100 --hop_len 50 --rnn_dropout 0. --use_gpu
 
 # w2v-msp
-python3 main.py --task personalisation --feature w2v-msp --emo_dim valence --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.005  --win_len 100 --hop_len 50 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature w2v-msp \
+            --emo_dim valence --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.005  \
+            --win_len 100 --hop_len 50 --rnn_dropout 0. --use_gpu
 
 # faus
-python3 main.py --task personalisation --feature faus --emo_dim valence --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.005  --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature faus \
+            --emo_dim valence --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.005  \
+            --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
 
 # vit
-python3 main.py --task personalisation --feature vit --normalize --emo_dim valence --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.001  --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature vit --normalize \
+            --emo_dim valence --model_dim 128 --rnn_n_layers 4 --rnn_bi --lr 0.001  \
+            --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
 
 # facenet
-python3 main.py --task personalisation --feature facenet --emo_dim valence --model_dim 128 --rnn_n_layers 2 --lr 0.005  --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
+python3 main.py --task personalisation --feature facenet \
+            --emo_dim valence --model_dim 128 --rnn_n_layers 2 --lr 0.005  \
+            --win_len 200 --hop_len 100 --rnn_dropout 0. --use_gpu
