7e43a145e028862914eda957359d5482e7ff1e63
GPHuang <gphuang@users.noreply.github.com>
2024-02-04 19:27:20 +0200

    Update cross_modal.py

--- cross_modal.py
+++ cross_modal.py
@@ -31,7 +31,7 @@
 # - rebuild muse conda env
 # gp is here
 # eval_c1_mimic.sh # done   
-# eval_c2_humor.sh # 27641193 cpu 
+# eval_c2_humor.sh # done 
 # eval_c3_affect.sh # done 
 # eval_c4_inversion.sh # done
 
@@ -55,9 +55,9 @@ python compare_systems.py /teamwork/t40511/muse_2023/exp/MuSe-2023-Aalto-models/
 # derive biosignals with '--predict' in main.py from avfeats for other tasks e.g. humor, mimic
 # derive affects (approval/disappoitment/uncertainty, humor, arousal/valence) with '--predict' in main.py 
 # hyper parameter search
-# bi-directional rnn
-# convolutional recurrent network
-# lstm, attention
+# bi-directional rnn gru lstm
+# convolutional recurrent network (crnn28076081) (rnn28076080)
+# attention
 
 ## 6. Results 
 # analyse_results.py
