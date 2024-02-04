df87856d139940d65852c0ae2c9358dc7b05f48b
GP <gpuhuang@gmail.com>
2024-02-04 19:23:44 +0200

    revert to prev commit

---
+++ cross_modal-copy.py
@@ -0,0 +1,73 @@
+## Setup
+# conda create -n muse python=3.10
+# conda activate muse
+# conda install pip
+# conda clean --all
+# pip cache purge
+# pip install -r requirements.txt --no-cache-dir
+
+## 0. Data exploration
+# - feat-feat, feat-label correlation analysis?
+# - baseline paper description of stats and results
+
+## 1. Baselines
+# av-feats to emotion_class (approval/disappoitment/uncertainty) - c1_mimic.sh
+# av-feats to humor_class (presence or absence of humor) - c2_humor.sh (c2)
+# av-feats & biosignals (BPM, ECG, resp) to emotion_values (arousal/valence) - [personalisation, affect]_[1st, 2nd]_step_best.sh (c3)
+# av-feats to biosignals - c4_avfeat2bio_[1st, 2nd]_step.sh (c4)
+# biosignals to biosignals - c5_bio2bio_[1st, 2nd]_step.sh (c5)
+
+## 2. Derive biosignals (default RNN setup)
+# - python data_preprocessing.py && copy [ECG, BPM, resp]_normalized to label(_segments) folder
+# - av-feats to biosignals (bpm, ecg, resp) (c4)
+# - normalized BPM ECG resp as features and targets (c5)
+# - biosignal as wav
+# - smoothing? 
+# - how biosignals & emotion_values are 'personalized'?
+# - view loss vs. feat, acc. vs. feat
+# - view training history? loss vs. feat, acc. vs. feat
+
+## 3. Evaluate '--predict' CodaLab
+# - rebuild muse conda env
+# gp is here
+# eval_c1_mimic.sh # done   
+# eval_c2_humor.sh # 27641193 cpu 
+# eval_c3_affect.sh # done 
+# eval_c4_inversion.sh # done
+
+## 4. Fusion/Cross-modal/Multi-modal
+# av-feats, derived emotion_values, derived biosignals to emotion_class (c1)
+# - used trained models from c3 to derive emotion_values and biosigals
+# - prep & preprocess (?) derived features as input for c1
+# - train & eval
+python late_fusion.py --task mimic \
+                    --model_ids []\
+                    --seeds []
+
+## 5. compare systems
+# ? predict on new data c1~c4
+# ? get class-wise performance for mimic & affect
+# error analysis, are systems complementary (go to fusion)
+python compare_systems.py /teamwork/t40511/muse_2023/exp/MuSe-2023-Aalto-models/results/prediction_muse/humor/RNN_2023-06-13-12-03_[bert-mono-en]_[128_4_False_64]_[0.001_256]/105/ \
+                          /teamwork/t40511/muse_2023/exp/MuSe-2023-baselines/results/prediction_muse/humor/RNN_2023-06-02-16-39_[bert-multilingual]_[128_4_False_64]_[0.001_256]/101/
+
+## 5. Deep learning with biosigs, avfeats, affects
+# derive biosignals with '--predict' in main.py from avfeats for other tasks e.g. humor, mimic
+# derive affects (approval/disappoitment/uncertainty, humor, arousal/valence) with '--predict' in main.py 
+# hyper parameter search
+# bi-directional rnn
+# convolutional recurrent network
+# lstm, attention
+
+## 6. Results 
+# analyse_results.py
+# affect does not use text-based features
+task='personalisation'
+for out in physio-arousal valence 
+do 
+for var in egemaps ds w2v-msp faus vit facenet biosignals resp BPM ECG 
+do
+ls ./results/log_muse/$task/RNN_*$var*$out*.txt
+less ./results/log_muse/$task/*$var*$out*.txt | grep 'Best' # Best Mean Pearsons
+done
+done
---
+++ run1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --time=01:30:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
+#SBATCH --output=logs/c3_1st_step_%A.out
+#SBATCH --job-name=muse
+#SBATCH -n 1
+
+module load miniconda
+
+source activate muse 
+
+# add cnn layer
+
+python3 main.py --task personalisation --feature BPM \
+                    --emo_dim physio-arousal \
+                    --model_type RNN \
+                    --model_dim 256 --rnn_n_layers 1 \
+                    --lr 0.001 --win_len 25 --hop_len 10 \
+                    --rnn_dropout 0.5 \
+                    --use_gpu
+
+                
