8205e1b777782deea3b22c303caf708422e34304
GPHuang <gphuang@users.noreply.github.com>
2024-02-04 18:49:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
-#SBATCH --output=logs/c3_1st_step_%A.out
+#SBATCH --output=logs/%A.out
 #SBATCH --job-name=muse
 #SBATCH -n 1
 
