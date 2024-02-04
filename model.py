2227a5e3cea4f6b0d1008bf61f3bcef4c0966fa0
GPHuang <gphuang@users.noreply.github.com>
2024-02-04 18:34:25 +0200

    Update model.py

--- model.py
+++ model.py
@@ -22,26 +22,24 @@ class Model(nn.Module):
         self.params = params
 
         self.inp = nn.Linear(params.d_in, params.model_dim, bias=False)
-        
-        # TODO
-        # CRNN
 
         self.encoder = CRNN(params.model_dim, params.model_dim, n_layers=params.rnn_n_layers, bi=params.rnn_bi,
                            dropout=params.rnn_dropout, n_to_1=params.n_to_1)
+
         d_rnn_out = params.model_dim * 2 if params.rnn_bi and params.rnn_n_layers > 0 else params.model_dim
         self.out = OutLayer(d_rnn_out, params.d_fc_out, params.n_targets, dropout=params.linear_dropout)
         self.final_activation = ACTIVATION_FUNCTIONS[params.task]()
 
     def forward(self, x, x_len):
-        print(x_len.shape)
-        print(x_len)
-        print(x.shape)
+        #print(x_len.shape)
+        #print(x_len)
+        #print(x.shape)
         x = self.inp(x)
-        print(x.shape)
+        #print(x.shape)
         x = self.encoder(x, x_len)
-        print(x.shape)
+        #print(x.shape)
         y = self.out(x)
-        print(x.shape)
+        #print(x.shape)
         activation = self.final_activation(y)
         return activation, x
 
