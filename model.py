cd0ddbade1c649ef0ee4a54060a818ffee8c9c27
GPHuang <gphuang@users.noreply.github.com>
2024-02-04 18:44:48 +0200

    Update model.py

--- model.py
+++ model.py
@@ -25,13 +25,14 @@ class Model(nn.Module):
 
        
         if params.model_type == 'CRNN':
-            myModel = CRNN(params.model_dim, params.model_dim)
+            self.encoder = CRNN(params.model_dim, params.model_dim, n_layers=params.rnn_n_layers, bi=params.rnn_bi,
+                           dropout=params.rnn_dropout, n_to_1=params.n_to_1)
         else:
-            myModel = RNN(params.model_dim, params.model_dim)
+            self.encoder = RNN(params.model_dim, params.model_dim, n_layers=params.rnn_n_layers, bi=params.rnn_bi,
+                           dropout=params.rnn_dropout, n_to_1=params.n_to_1)
             import sys
             sys.exit(0)
-        self.encoder = myModel(n_layers=params.rnn_n_layers, bi=params.rnn_bi,
-                           dropout=params.rnn_dropout, n_to_1=params.n_to_1)
+        
 
         d_rnn_out = params.model_dim * 2 if params.rnn_bi and params.rnn_n_layers > 0 else params.model_dim
         self.out = OutLayer(d_rnn_out, params.d_fc_out, params.n_targets, dropout=params.linear_dropout)
